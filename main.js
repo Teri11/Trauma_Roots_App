6af7e58a1b5f6bae7ad502b442a932b2dbb55159
Teri11 <augustineteri157@gmail.com>
2022-09-10 18:32:12 +0300

    bulk commit

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-// alert("We would Like to show \n you notifications for the latest \n news & updates on \n Trauma Roots."); 
+// alert("We would Like to show \n you notifications for the latest \n news & updates on \n Trauma Roots.");
+$("#myCarousel").carousel({
+  interval: false
+});
